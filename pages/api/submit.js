a44984290e4fda50e07a58637c026ff33c054e51
Nafiz iqbal <nafiziqbal.mo@gmail.com>
2024-10-23 19:03:06 +0600

    testing

--- pages/api/submit.js
+++ pages/api/submit.js
@@ -1,29 +1,23 @@
-import bdtickets from "./bdTickets";
 import sohojTickets from "./sohojTickets";
+import bdtickets from "./bdtickets";
 
 export default async function handler(req, res) {
+  console.log("Received request:", req.method, req.body); // Log the request method and body
+
   if (req.method === "POST") {
     const { from, to, date } = req.body;
 
+    // Split the date into year, month, and day
     const [year, month, day] = date.split("-");
 
-    // Call the bdtickets function and wait for the result
     try {
       // Use Promise.all to call both functions concurrently
       const [sohojResult, bdTicketResult] = await Promise.all([
         sohojTickets(from.toLowerCase(), to.toLowerCase(), year, month, day),
         bdtickets(from.toLowerCase(), to.toLowerCase(), year, month, day),
       ]);
-      // Combine the results
-      // const combinedResult = {
-      //   bdtickets: bdTicketResult,
-      //   sohojTickets: sohojResult,
-      // };
-    const combinedResult = [...bdTicketResult, ...sohojResult];
 
-
-     
-
+      const combinedResult = [...bdTicketResult, ...sohojResult];
       console.log("Scraped Data:", combinedResult); // Log the scraped data
 
       // Respond with a success message and the result
@@ -39,13 +33,10 @@ export default async function handler(req, res) {
       });
     } catch (error) {
       console.error("Error processing data:", error);
-      res
-        .status(500)
-        .json({ message: "Internal Server Error", error: error.message });
+      res.status(500).json({ message: "Internal Server Error", error: error.message });
     }
   } else {
     // Handle any other HTTP methods
     res.status(405).json({ message: "Method not allowed" });
   }
 }
-// pages/api/submit.js 
